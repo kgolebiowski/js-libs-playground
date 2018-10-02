1b80fc74ba7d4b6ce1e63e2c4fb24f66f68a79af
Krzysztof Golebiowski <krzysiek.golebiowski@gmail.com>
2018-10-02 19:38:11 +0200

    Created standard Liferay MVC portlet with command "blade create -t mvc-portlet shared-js-libs-portlet"

---
+++ modules/shared-js-libs-portlet/src/main/java/shared/js/libs/portlet/constants/SharedJsLibsPortletKeys.java
@@ -0,0 +1,10 @@
+package shared.js.libs.portlet.constants;
+
+/**
+ * @author kgolebiowski
+ */
+public class SharedJsLibsPortletKeys {
+
+	public static final String SharedJsLibs = "sharedjslibs";
+
+}
---
+++ modules/shared-js-libs-portlet/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,5 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="sharedjslibs.caption"/></b>
+</p>
